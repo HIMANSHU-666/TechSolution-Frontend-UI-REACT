e8a807318ef364fe8713d6a8bbce84946d69df4a
HIMANSHU-666 <sharma.himanshu1324@gmail.com>
2023-05-26 07:58:48 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# TechSolution-Frontend-UI-REACT 
+
+Hi! My name is **Himanshu**, I have created this web design for getting perfection in REACT.
+
+
+# Install Dependencies
+
+**command** - `npm i`
+
+
+## Author
+
+**Instagram** Click [Here](https://www.instagram.com/am_himanshusharma/?igshid=ZmZhODViOGI%3D) **@am_himanshusharma**
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/footer.scss";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div>
+          <h1>TechSolution</h1>
+          <p>@all rights are reserved</p>
+        </div>
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="####">Youtube</a>
+                <a href="####">Instagram</a>
+                <a href="####">Github</a>
+                
+            </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
